8849d6acb064ad05a6e8b98421042b369efddbc8
mikey83mikey83-42 <mikey83mikey83@gmail.com>
2026-04-15 18:12:59 -0400

    Update main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def read_root():
     return {"Hello": "World"}
 
 if __name__ == "__main__":
-    # This allows you to run 'python Main.py' 
+    # This allows you to run 'python main.py' 
     # instead of using the uvicorn command line
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
      
