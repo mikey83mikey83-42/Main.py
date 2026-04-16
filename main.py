a2bdc386db07166a0f09de6ed627e3c0fb80123d
mikey83mikey83-42 <mikey83mikey83@gmail.com>
2026-04-15 20:27:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-if __name__ == "__main.py__":
+if __name__ == "__main__":
     # This allows you to run 'python main.py' 
     # instead of using the uvicorn command line
     uvicorn.run("main.py:app", host="0.0.0.0", port=8000, reload=True)
