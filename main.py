9b53f00db8a3f8d6b8e42499f04540a396ff1240
mikey83mikey83-42 <mikey83mikey83@gmail.com>
2026-04-16 02:46:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-
 app = FastAPI()
-
 @app.get("/")
 def read_root():
     return {"status": "success", "message": "Your FastAPI app is running on Render!"}
-
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
